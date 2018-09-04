44ba78cd14a11fd552aeb850d40262eed2acad1c
dduraess <davison.duraes@gmail.com>
2018-09-04 12:37:33 -0300

    ajuste nos testes de comércio

--- Comercio/src/org/comun/comercio/CompraTest.java
+++ Comercio/src/org/comun/comercio/CompraTest.java
@@ -1,12 +1,25 @@
 package org.comun.comercio;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CompraTest {
 
 	@Test
 	public void compraAVistaTest() {
-//		fail("Not yet implemented");
+		Compra novaCompraAVista = new Compra(500);
+		assertEquals(500, novaCompraAVista.getValorTotal());
+		assertEquals(500, novaCompraAVista.getValorParcela());
+		assertEquals(1, novaCompraAVista.getNrParcelas());
+	}
+
+	@Test
+	public void compraParceladaTest() {
+		Compra novaCompraParcelada = new Compra(5, 100);
+		assertEquals(500, novaCompraParcelada.getValorTotal());
+		assertEquals(100, novaCompraParcelada.getValorParcela());
+		assertEquals(5, novaCompraParcelada.getNrParcelas());
 	}
 
 }
