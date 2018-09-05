ed49218285c8c253bd767ea54c446ef36eac98a2
dduraess <aluno@SERPRO-RLSL\10.32.18.180>
2018-09-05 08:48:50 -0300

    ajustes na classe imc

--- IMC/src/org/comun/imc/Paciente.java
+++ IMC/src/org/comun/imc/Paciente.java
@@ -19,32 +19,32 @@ public class Paciente {
 		
 		double valorIMC = this.calcularIMC(peso, altura);
 		String seuDiagnostico;
-		double faixa1 = 16/altura * altura;
-		double faixa2 = 16.99/altura * altura;
-		double faixa3 = 17/altura * altura;
-		double faixa4 = 18.49/altura * altura;
-		double faixa5 = 18.50/altura * altura;
-		double faixa6 = 24.99/altura * altura;
-		double faixa7 = 25/altura * altura;
-		double faixa8 = 29.99/altura * altura;
-		double faixa9 = 30/altura * altura;
-		double faixa10 = 34.99/altura * altura;
-		double faixa11 = 35/altura * altura;
-		double faixa12 = 39.99/altura * altura;
+		double faixa1 = 16;
+		double faixa2 = 16.99;
+		double faixa3 = 17;
+		double faixa4 = 18.49;
+		double faixa5 = 18.50;
+		double faixa6 = 24.99;
+		double faixa7 = 25;
+		double faixa8 = 29.99;
+		double faixa9 = 30;
+		double faixa10 = 34.99;
+		double faixa11 = 35;
+		double faixa12 = 39.99;
 		
 		if (valorIMC < faixa1) {
 			seuDiagnostico = "Baixo peso muito grave";
-		} else if (valorIMC > faixa1 && valorIMC < faixa2) {
+		} else if (valorIMC >= faixa1 && valorIMC <= faixa2) {
 			seuDiagnostico = "Baixo peso grave";
-		} else if (valorIMC > faixa3 && valorIMC < faixa4) {
+		} else if (valorIMC >= faixa3 && valorIMC <= faixa4) {
 			seuDiagnostico = "Baixo peso";
-		} else if (valorIMC > faixa5 && valorIMC < faixa6) {
+		} else if (valorIMC >= faixa5 && valorIMC <= faixa6) {
 			seuDiagnostico = "Peso normal";
-		} else if (valorIMC > faixa7 && valorIMC < faixa8) {
+		} else if (valorIMC >= faixa7 && valorIMC <= faixa8) {
 			seuDiagnostico = "Sobrepeso";
-		} else if (valorIMC > faixa9 && valorIMC < faixa10) {
+		} else if (valorIMC >= faixa9 && valorIMC <= faixa10) {
 			seuDiagnostico = "Obesidade grau I";
-		} else if (valorIMC > faixa11 && valorIMC < faixa12) {
+		} else if (valorIMC >= faixa11 && valorIMC <= faixa12) {
 			seuDiagnostico = "Obesidade grau II";
 		} else {
 			seuDiagnostico = "Obesidade grau III";
