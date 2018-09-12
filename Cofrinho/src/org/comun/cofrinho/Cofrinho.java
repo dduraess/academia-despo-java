b10f6d3fd2bf872a21f76d2559cd38fd135908f1
dduraess <davison.duraes@gmail.com>
2018-09-12 14:47:15 -0300

    exercício cofrinho

--- Cofrinho/src/org/comun/cofrinho/Cofrinho.java
+++ Cofrinho/src/org/comun/cofrinho/Cofrinho.java
@@ -1,34 +1,78 @@
 package org.comun.cofrinho;
 
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cofrinho {
-	
-	List<Moeda> moedasArmazenadas;
+
+	List<Moeda> moedasArmazenadas = new ArrayList<>();
+
+	private int moedasDeCinco=0;
+	private int moedasDeDez=0;
+	private int moedasDeVinteCinco=0;
+	private int moedasDeCinquenta=0;
+	private int moedasDeUm=0;
 	
 	public void receberMoeda(Moeda moeda) {
 		moedasArmazenadas.add(moeda);
 	}
 	
 	public int contarMoedas(List<Moeda> moedas){
+		
 		int total=0;
-		for (Iterator iterator = moedas.iterator(); iterator.hasNext();) {
-//			Moeda moeda = (Moeda) iterator.next();
+		
+		for (int i = 0; i < moedas.size(); i++) {
 			total++;
 		} return total;
+		
 	}
 	
 	public void contarMoedasPorValor (List<Moeda> moedas) {
-		int moedasDeCinco;
-		int moedasDeDez;
-		int moedasDeVinteCinco;
-		for (Iterator iterator = moedas.iterator(); iterator.hasNext();) {
-			Moeda moeda = (Moeda) iterator.next();
-			if (moeda.valor==0.05) {
-				
+
+		if (!moedas.isEmpty()) {
+			for (int i = 0; i < moedas.size(); i++) {
+				if (moedas.get(i).valor.equals(0.05)) {
+					moedasDeCinco++;
+				} else if (moedas.get(i).valor.equals(0.1)) {
+					moedasDeDez++;
+				} else if (moedas.get(i).valor.equals(0.25)) {
+					moedasDeVinteCinco++;
+				} else if (moedas.get(i).valor.equals(0.5)) {
+					moedasDeCinquenta++;
+				} else if (moedas.get(i).valor.equals(1.0)) {
+					moedasDeUm++;
+				}
 			}
 		}
+
+	}
+	
+	public double informarMoedaMaiorValor () {		
+		
+		if (moedasDeUm > 0) {
+			return 1;
+		} else if (moedasDeCinquenta > 0) {
+			return 0.5;
+		} else if (moedasDeVinteCinco > 0) {
+			return 0.25;
+		} else if (moedasDeDez > 0) {
+			return 0.1;			
+		} else if (moedasDeCinco > 0) {
+			return 0.05;
+		} else {
+			return 0;
+		}
+	}
+	
+	public void imprimirContabilidadeCofrinho () {
+		contarMoedasPorValor(moedasArmazenadas);
+		System.out.println("Moedas de cinco: " + moedasDeCinco);
+		System.out.println("Moedas de dez: " + moedasDeDez);
+		System.out.println("Moedas de vinte e cinco: " + moedasDeVinteCinco);
+		System.out.println("Moedas de cinquenta: " + moedasDeCinquenta);
+		System.out.println("Moedas de um: " + moedasDeUm);
+		System.out.println("Total de moedas: " + contarMoedas(moedasArmazenadas));
+		System.out.println("Moeda de maior valor: " + informarMoedaMaiorValor());
 	}
 
 }
---
+++ Cofrinho/src/org/comun/cofrinho/TestarCofrinho.java
@@ -0,0 +1,33 @@
+package org.comun.cofrinho;
+
+public class TestarCofrinho {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Moeda moeda1 = new Moeda(0.1);
+//		Moeda moeda2 = new Moeda(0.1);
+//		Moeda moeda3 = new Moeda(0.05);
+//		Moeda moeda4 = new Moeda(0.25);
+//		Moeda moeda5 = new Moeda(0.05);
+//		Moeda moeda6 = new Moeda(0.05);
+//		Moeda moeda7 = new Moeda(0.1);
+//		Moeda moeda8 = new Moeda(0.1);
+//		Moeda moeda9 = new Moeda(0.1);
+		
+		Cofrinho cofrinho = new Cofrinho();
+		
+//		cofrinho.receberMoeda(moeda1);
+//		cofrinho.receberMoeda(moeda2);
+//		cofrinho.receberMoeda(moeda3);
+//		cofrinho.receberMoeda(moeda4);
+//		cofrinho.receberMoeda(moeda5);
+//		cofrinho.receberMoeda(moeda6);
+//		cofrinho.receberMoeda(moeda7);
+//		cofrinho.receberMoeda(moeda8);
+//		cofrinho.receberMoeda(moeda9);
+		
+		cofrinho.imprimirContabilidadeCofrinho();
+
+	}
+
+}
