4d9dec828a8badad072212b02704791550f4c94f
aluno <aluno@10.32.18.180>
2018-09-19 09:59:51 -0300

    ajuste testes

--- Cofrinho/src/Test/TestarCofrinho.java
+++ Cofrinho/src/Test/TestarCofrinho.java
@@ -17,36 +17,27 @@ public class TestarCofrinho {
 	}
 	
 	Cofrinho cofrinho;
-	Moeda moeda1;
-	Moeda moeda2;
-	Moeda moeda3;
-	Moeda moeda4;
-	Moeda moeda5;
-	Moeda moeda6;
-	Moeda moeda7;
-	Moeda moeda8;
-	Moeda moeda9;
 
 	@Before
 	public void criarCofrinhoBefore() {
 
 		cofrinho = new Cofrinho();
-		cofrinho.receberMoeda(moeda1 = new Moeda(0.1));
-		cofrinho.receberMoeda(moeda2 = new Moeda(0.1));
-		cofrinho.receberMoeda(moeda3 = new Moeda(0.05));
-		cofrinho.receberMoeda(moeda4 = new Moeda(0.25));
-		cofrinho.receberMoeda(moeda5 = new Moeda(0.05));
-		cofrinho.receberMoeda(moeda6 = new Moeda(0.05));
-		cofrinho.receberMoeda(moeda7 = new Moeda(0.1));
-		cofrinho.receberMoeda(moeda8 = new Moeda(0.1));
-		cofrinho.receberMoeda(moeda9 = new Moeda(0.1));
+		cofrinho.receberMoeda(new Moeda(0.1));
+		cofrinho.receberMoeda(new Moeda(0.1));
+		cofrinho.receberMoeda(new Moeda(0.05));
+		cofrinho.receberMoeda(new Moeda(0.25));
+		cofrinho.receberMoeda(new Moeda(0.05));
+		cofrinho.receberMoeda(new Moeda(0.05));
+		cofrinho.receberMoeda(new Moeda(0.1));
+		cofrinho.receberMoeda(new Moeda(0.1));
+		cofrinho.receberMoeda(new Moeda(0.1));
 		System.out.println("@Before");
 
 	}
 
 	@Test
 	public void testarTotalDepositado() {
-		assertEquals(0.9, cofrinho.contarValorTotalCofrinho(), 0.9);
+		assertEquals(0.9, cofrinho.contarValorTotalCofrinho(), 0.0);
 
 	}
 
@@ -58,7 +49,7 @@ public class TestarCofrinho {
 
 	@Test
 	public void testarInformarMoedasMaiorValor() {
-		assertEquals(0.25, cofrinho.informarMoedaMaiorValor(), 0.25);
+		assertEquals(0.25, cofrinho.informarMoedaMaiorValor(), 0.0);
 
 	}
 
