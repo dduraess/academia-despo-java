f82056aef2e11291746c9e4ade43788755a6a129
dduraess <davison.duraes@gmail.com>
2018-10-19 09:20:47 -0300

    COMMIT TV

--- TV/src/br/serpro/tv/TV.java
+++ TV/src/br/serpro/tv/TV.java
@@ -2,32 +2,64 @@ package br.serpro.tv;
 
 public class TV {
 	
-	private int canalAtivo = 0;
+	private int canalAtivo;
 	private int nrPossivelCanais;
-	private byte volume = 0;
-	private boolean estaLigada = false;
+	private int listaCanais[];
+	private byte volume;
+	private boolean estaLigada;
 	
 	public TV (int nrCanais) {
 		if (nrCanais>0) {
 			nrPossivelCanais = nrCanais;
+			int contadorVetor = 1;
+			for (int i = 0; i < nrPossivelCanais; i++) {
+				listaCanais[i] = contadorVetor;
+				contadorVetor++;
+			}
 		}
+		canalAtivo = 0;
+		volume = 0;
+		estaLigada = false;
 	}
 	
-	public void mudarCanalNr(int nrCanal) {
+	public int mudarCanalNr(int nrCanal) {
 		if (nrCanal > 0 && nrCanal <= nrPossivelCanais) {
-			canalAtivo = nrCanal;
+			canalAtivo = listaCanais[nrCanal-1];
 		}
+		return canalAtivo;
 	}
 	
-	public void aumentarCanal() {
-		if (canalAtivo++ < nrPossivelCanais) {
-			canalAtivo++;
+	public int aumentarCanal() {
+		if (listaCanais[canalAtivo] <= nrPossivelCanais) {
+			canalAtivo = listaCanais[canalAtivo];
 		}
+		return canalAtivo;
+	}
+	
+	public int diminuirCanal() {
+		if (listaCanais[canalAtivo-2] > 0) {
+			canalAtivo = listaCanais[canalAtivo-2];
+		}
+		return canalAtivo;
+	}
+	
+	public int aumentarVolume() {
+		volumeAtual = ;
+		if (volume++ <= 50) {
+			volume++;
+		}
+		return volume;
+	}
+	
+	public int diminuirVolume() {
+		if (volume-- > 0) {
+			volume--;
+		}
+		return volume;
 	}
 	
 	public void ligaDesliga() {
 		estaLigada=!estaLigada;
 	}
-	
 
 }
