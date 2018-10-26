857b5f9e0f20f6774c6e206ba0c44c175b563aaa
dduraess <davison.duraes@gmail.com>
2018-10-26 12:14:35 -0200

    merge jogador

--- Goleador2018/src/br/gov/serpro/ranking/Jogador.java
+++ Goleador2018/src/br/gov/serpro/ranking/Jogador.java
@@ -1,26 +1,23 @@
 package br.gov.serpro.ranking;
 
-import java.util.Collection;
-
 public class Jogador implements Comparable<Jogador> {
 	
 	private String nome;
 	private Integer nrGols;
 	
 	public Jogador(String nome, Integer nrGols) {
-		super();
 		this.nome = nome;
 		this.nrGols = nrGols;
 	}
 
 	@Override
 	public int compareTo(Jogador o) {
-		if (nrGols == o.nrGols) {
-			return 0;
+		if (nrGols > o.nrGols) {
+			return 1;
 		} 
 		if (nrGols < o.nrGols) {
 			return -1;
-		} return 1;
+		} return 0;
 	}
 
 	public String getNome() {
