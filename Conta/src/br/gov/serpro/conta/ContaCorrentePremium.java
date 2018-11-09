a57e7f67ff1633a977ba9d31e88efeec4e031ca7
aluno <aluno@10.32.18.180>
2018-11-09 08:59:21 -0200

    correção conta corrente

--- Conta/src/br/gov/serpro/conta/ContaCorrentePremium.java
+++ Conta/src/br/gov/serpro/conta/ContaCorrentePremium.java
@@ -1,8 +1,11 @@
 package br.gov.serpro.conta;
 
+import br.gov.serpro.conta.test.SaldoInsuficienteException;
+
 public class ContaCorrentePremium extends ContaCorrente {
 	
 	private Double saldo = 0.0; 
+	private static final Double LIMITE = 50.00;
 	private static final Double REMUNERACAO = 1.06;
 	private static final Double TAXA_DE_JUROS = 1.0;
 
@@ -19,6 +22,16 @@ public class ContaCorrentePremium extends ContaCorrente {
 	@Override
 	public Double calcularTaxaJuros() {
 		return TAXA_DE_JUROS;
+	}
+
+	@Override
+	public void efetuarRetirada(double valorRetirada) throws SaldoInsuficienteException {
+		if ((obterSaldo() + LIMITE)>= valorRetirada) {
+			saldo = saldo - valorRetirada; 
+		} else {
+			throw new SaldoInsuficienteException("Saldo insuficiente!");
+		}
+		
 	}
 
 }
--- Conta/test/br/gov/serpro/conta/test/RetiradaContaCorrenteRegularTest.java
+++ Conta/test/br/gov/serpro/conta/test/RetiradaContaCorrenteRegularTest.java
@@ -30,34 +30,34 @@ public class RetiradaContaCorrenteRegularTest {
         assertEquals(50.0, contaCorrente.obterSaldo(), 0);
     }
 
-//    @Test
-//    public void devePermitirComValorIgualAoSaldoTotal() {
-//
-//        // Given
-//        ContaCorrente contaCorrente = new ContaCorrenteRegular();
-//
-//        // When
-//        contaCorrente.depositar(100.0);
-//        try {
-//            contaCorrente.efetuarRetirada(100.0);
-//        } catch (SaldoInsuficienteException exception) {
-//            fail("Deveria permitir efetuar retirada com valor igual que saldo total");
-//        }
-//
-//        // Then
-//        assertEquals(0.0, contaCorrente.obterSaldo(), 0);
-//    }
-//
-//    @Test(expected = SaldoInsuficienteException.class)
-//    public void naoDevePermitirSaqueSuperiorAoValorDepositado() throws SaldoInsuficienteException {
-//
-//        // Given
-//        ContaCorrente contaCorrente = new ContaCorrenteRegular();
-//
-//        // When
-//        contaCorrente.depositar(50.0);
-//        contaCorrente.efetuarRetirada(50.01);
-//
-//    }
+    @Test
+    public void devePermitirComValorIgualAoSaldoTotal() {
+
+        // Given
+        ContaCorrente contaCorrente = new ContaCorrenteRegular();
+
+        // When
+        contaCorrente.depositar(100.0);
+        try {
+            contaCorrente.efetuarRetirada(100.0);
+        } catch (SaldoInsuficienteException exception) {
+            fail("Deveria permitir efetuar retirada com valor igual que saldo total");
+        }
+
+        // Then
+        assertEquals(0.0, contaCorrente.obterSaldo(), 0);
+    }
+
+    @Test(expected = SaldoInsuficienteException.class)
+    public void naoDevePermitirSaqueSuperiorAoValorDepositado() throws SaldoInsuficienteException {
+
+        // Given
+        ContaCorrente contaCorrente = new ContaCorrenteRegular();
+
+        // When
+        contaCorrente.depositar(50.0);
+        contaCorrente.efetuarRetirada(50.01);
+
+    }
 
 }
